7fc94fc3105003ab72e52a6e0439933e1bfb0a5c
JonatSrG <JonatSrG>
2022-02-04 10:44:40 -0600

    Usando API Interseccion Observer

--- src/index.js
+++ src/index.js
@@ -3,4 +3,37 @@
  * When you're ready to start on your site, clear the file. Happy hacking!
  **/
 
-console.log('Happy hacking:)')
+//funcion random de las imagenes
+const minimum = 1;
+const maximum = 122;
+const random = () => Math.floor(Math.random() * (maximum - minimum)) + minimum;
+//crear (1) imagenes
+const createImageNode = () => {
+
+    const container = document.createElement('div');
+    container.className = "p-4";
+
+    const imagen = document.createElement('img');
+    imagen.className = "mx-auto";
+    imagen.width = '320';
+    imagen.src = `https://randomfox.ca/images/${random()}.jpg`; //TODO
+
+    container.appendChild(imagen);
+
+    return container;
+};
+
+//agregar #imagen
+const nuevaImagen = createImageNode();
+const mountNode = document.getElementById("images");
+
+const addButton = document.querySelector("button");
+
+//funccion del boton de agregar
+const addImage = () => {
+    const newImage = createImageNode();
+    mountNode.append(newImage);
+};
+
+addButton.addEventListener("click", addImage);
+
